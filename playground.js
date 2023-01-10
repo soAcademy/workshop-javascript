1c47ed7cfb259d6176ba80d04d3727f4cff86fc7
mplug <oppoalien@gmail.com>
2023-01-10 11:23:17 +0700

    feat: run function workshop

--- playground.js
+++ playground.js
@@ -46,10 +46,31 @@
 // ทำงานไม่ได้ แต่ไขแล้ว
 
 // no.3
-const a = 10;
-function greet() {
-    const b = 20;
-    console.log(a + b);
-}
-greet();
+// const a = 10;
+// function greet() {
+//     const b = 20;
+//     console.log(a + b);
+// }
+// greet();
 // ทำงานไม่ได้ แต่ไขแล้ว
+
+//Functions Workshop
+//1.
+function sum1(a, b, c) {
+    return a + b + c;
+}
+console.log(sum1(1, 2, 3));
+
+//2.
+const sum2 = (a, b, c) => a + b + c;
+console.log(sum1(1, 2, 3));
+
+//3.
+const sum3 = (a, b, c) => {
+    return { x: a.x + b.x + c.x, y: a.y + b.y + c.y };
+};
+console.log(sum3({ x: 1, y: 1 }, { x: 2, y: 2 }, { x: 3, y: 3 }));
+
+//4.
+const isOdd = (val) => val % 2 !== 0;
+console.log(isOdd(5));
