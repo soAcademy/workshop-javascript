fd3caa400d74db480caff495fbdc2b78269c9b1f
Poom <poomhan@gmail.com>
2023-01-10 10:44:04 +0700

    edit greet

--- playground.js
+++ playground.js
@@ -1,4 +1,4 @@
-console.log("Hello World");
+/* console.log("Hello World");
 const a = 1;
 const b = "hello";
 const c = true;
@@ -9,8 +9,29 @@ const myArray = [1, 2, 3];
 const myObject = [
   { x: 1, y: 2 },
   { x: 3, y: 4 },
-];
+]; */
 
-console.log(a, b, c, d, e);
+/* console.log(a, b, c, d, e);
 console.log(myArray);
-console.log(myObject);
+console.log(myObject); */
+
+
+const a1 = "hello";
+function greet() {
+  console.log(a1);
+}
+greet();
+
+let a2 = "hello";
+function greet2() {
+  a2 = "hi";
+}
+console.log(a2);
+
+const a = 10;
+function greet3() {
+  const b = 20;
+  console.log(a + b);
+}
+greet3();
+
