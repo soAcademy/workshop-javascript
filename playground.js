866122aeaca7231472d51315b1628bcb8bc9c1dc
Poom <poomhan@gmail.com>
2023-01-10 09:59:13 +0700

    init

--- playground.js
+++ playground.js
@@ -1,1 +1,13 @@
 console.log("Hello World");
+const a = 1;
+const b = "hello";
+const c = true;
+const d = 10;
+let e = 10;
+e = 20;
+const myArray = [1,2,3]
+const myObject = [{x:1, y:2}, {x:3,y:4}]
+
+console.log(a, b, c,d,e)
+console.log(Array)
+console.log(myObject);
