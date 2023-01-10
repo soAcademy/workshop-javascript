12f8fab465e651ccb16e2fb3b89efd7bfbf7be9a
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-01-10 09:39:48 +0700

    feat: jsc test playground

--- playground.js
+++ playground.js
@@ -1,1 +1,13 @@
 console.log("Hello World");
+const a = 1;
+const b = "hello";
+const c = true;
+const d = 10;
+let e = 10;
+e = 20;
+const myArray = [1, 2, 3];
+const myObject = { x: 1, y: 2 };
+const myObjects = [
+  { x: 1, y: 2 },
+  { x: 3, y: 4 },
+];
