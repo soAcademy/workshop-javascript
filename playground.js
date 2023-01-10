2955858d29f555ad378013acf6b2a0587a190a51
jaiieth <midzty.rb128@gmail.com>
2023-01-10 13:30:28 +0700

    feat:flow control workshop

--- playground.js
+++ playground.js
@@ -66,35 +66,75 @@
 // );
 // console.log("console.log() has to be inside greet() to work properly\n");
 
-// Function Workshop
-console.log("Function Workshop");
+// // Function Workshop
+// console.log("Function Workshop");
+// // Q1
+// function sum1(a, b, c) {
+// 	return a + b + c;
+// }
+// console.log(sum1(1, 2, 3));
+
+// // Q2
+// const sum2 = (a, b, c) => a + b + c;
+// console.log(sum2(1, 2, 3));
+
+// // Q3
+// const sum3 = (a, b, c) => {
+// 	return {
+// 		x: a.x + b.x + c.x,
+// 		y: a.y + b.y + c.y,
+// 	};
+// };
+// let a = { x: 1, y: 2 };
+// let b = { x: 3, y: 4 };
+// let c = { x: 5, y: 6 };
+// console.log(sum3(a, b, c));
+
+// // Q4
+// const isOdd = (x) => {
+// 	return x % 2 != 0;
+// };
+// const isOdd2 = (x) => x % 2 != 0;
+
+// console.log(isOdd(153));
+// console.log(isOdd2(14));
+
+// Flow Control Workshop
+console.log("Flow Control Workshop");
 // Q1
-function sum1(a, b, c) {
-	return a + b + c;
-}
-console.log(sum1(1, 2, 3));
+const calculateBMI = (weight, height) => {
+	const BMI = (weight / (height / 100) ** 2).toFixed(2);
+	return (grade =
+		BMI < 18.5
+			? "underweight"
+			: BMI < 25
+			? "healthy"
+			: BMI < 30
+			? "overweight"
+			: "obesity");
+};
+console.log(calculateBMI(60, 170));
 
 // Q2
-const sum2 = (a, b, c) => a + b + c;
-console.log(sum2(1, 2, 3));
+// For loop
+const average = (input) => {
+	let total = 0;
+	for (let i = 0; i < input.length; i++) {
+		total += input[i];
+	}
+	return total / input.length;
+};
+console.log(average([1, 2, 3, 4]));
 
 // Q3
-const sum3 = (a, b, c) => {
-	return {
-		x: a.x + b.x + c.x,
-		y: a.y + b.y + c.y,
-	};
+const sumOdd = (input) => {
+	let total = 0;
+	for (let i = 0; i < input.length; i++) {
+		if (input[i] % 2 != 0) {
+			total += input[i];
+		}
+	}
+	return total;
 };
-let a = { x: 1, y: 2 };
-let b = { x: 3, y: 4 };
-let c = { x: 5, y: 6 };
-console.log(sum3(a, b, c));
 
-// Q4
-const isOdd = (x) => {
-	return x % 2 != 0;
-};
-const isOdd2 = (x) => x % 2 != 0;
-
-console.log(isOdd(153));
-console.log(isOdd2(14));
+console.log(sumOdd([1, 8, 9, -3, 0, 4]));
