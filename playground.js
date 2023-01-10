206466bcc78b9f9b4fdae2309497920ad0e41f00
kunlanis <yoo.kunlanis@gmail.com>
2023-01-10 09:36:14 +0700

    add variable

--- playground.js
+++ playground.js
@@ -1,1 +1,21 @@
 console.log("Hello World");
+//1
+const a = 1;
+//2
+const b = "Hello";
+//3
+const c = true;
+//4
+const d = 10;
+//5
+let e = 20;
+e = 20;
+//6
+const myArray = [1, 2, 3];
+//7
+const myObject = { x: 1, y: 2 };
+//8
+const myObjects = [
+  { x: 1, y: 2 },
+  { x: 3, y: 4 },
+];
