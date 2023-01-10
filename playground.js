043b5a497565d2e04aff838e227e8b809e029970
Phisit Siprasatthong <phisite@gmail.com>
2023-01-10 09:51:00 +0700

    feat: add output

--- playground.js
+++ playground.js
@@ -10,4 +10,16 @@ let e = 10;
 e = 20;
 const myArray = [1, 2, 3];
 const myObject = { x: 1, y: 2 };
-const myObjects = [{ x: 1, y: 2 }, { x: 3, y: 4 }]
+const myObjects = [
+  { x: 1, y: 2 },
+  { x: 3, y: 4 },
+];
+
+console.log("a = " + a);
+console.log("b = " + b);
+console.log("c = " + c);
+console.log("d = " + d);
+console.log("e = " + e);
+console.log("myArray = " + myObjects);
+console.log("myObject = " + myObject);
+console.log("myObjects = " + myObjects);
