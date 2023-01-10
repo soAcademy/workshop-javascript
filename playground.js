3e3e607350b974bd91de109e2b4b19517996539b
Phisit Siprasatthong <phisite@gmail.com>
2023-01-10 11:15:16 +0700

    feat: comment out previous w/s and add functions w/s #1

--- playground.js
+++ playground.js
@@ -1,56 +1,64 @@
 console.log("Hello World");
 
-console.log("Variables Workshop");
+// console.log("Variables Workshop");
 
-const a = 1;
-const b = "hello";
-const c = true;
-const d = 10;
-let e = 10;
-e = 20;
-const myArray = [1, 2, 3];
-const myObject = { x: 1, y: 2 };
-const myObjects = [
-  { x: 1, y: 2 },
-  { x: 3, y: 4 },
-];
+// const a = 1;
+// const b = "hello";
+// const c = true;
+// const d = 10;
+// let e = 10;
+// e = 20;
+// const myArray = [1, 2, 3];
+// const myObject = { x: 1, y: 2 };
+// const myObjects = [
+//   { x: 1, y: 2 },
+//   { x: 3, y: 4 },
+// ];
 
-console.log("a = " + a);
-console.log("b = " + b);
-console.log("c = " + c);
-console.log("d = " + d);
-console.log("e = " + e);
-console.log("myArray = " + myObjects);
-console.log("myObject = " + myObject);
-console.log("myObjects = " + myObjects);
+// console.log("a = " + a);
+// console.log("b = " + b);
+// console.log("c = " + c);
+// console.log("d = " + d);
+// console.log("e = " + e);
+// console.log("myArray = " + myObjects);
+// console.log("myObject = " + myObject);
+// console.log("myObjects = " + myObjects);
 
-console.log("Scope Workshop #1");
+// console.log("Scope Workshop #1");
 
-const aa = "hello";
+// const aa = "hello";
 
-function greet() {
-  console.log(aa);
+// function greet() {
+//   console.log(aa);
+// }
+
+// greet();
+
+// console.log("Scope Workshop #2");
+
+// let aaa = "hello"; // fixed
+
+// function greet1() {
+//   aaa = "hi";
+// }
+
+// console.log(aaa);
+
+// console.log("Scope Workshop #3");
+
+// const aaaa = 10;
+
+// function greet2() {
+//   const bbbb = 20;
+//   console.log(aaaa + bbbb); // fixed
+// }
+
+// greet2();
+
+console.log("Funnctions Workshop #1");
+
+function sum1(a, b, c) {
+  return a + b + c;
 }
 
-greet();
-
-console.log("Scope Workshop #2");
-
-let aaa = "hello"; // fixed
-
-function greet1() {
-  aaa = "hi";
-}
-
-console.log(aaa);
-
-console.log("Scope Workshop #3");
-
-const aaaa = 10;
-
-function greet2() {
-  const bbbb = 20;
-  console.log(aaaa + bbbb); // fixed
-}
-
-greet2();
+console.log(sum1(1, 2, 3));
