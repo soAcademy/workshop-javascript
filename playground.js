156d273e9308f49e27bf8a47a79a97afe3ebe702
jaiieth <midzty.rb128@gmail.com>
2023-01-10 09:34:35 +0700

    fix: code formatting

--- playground.js
+++ playground.js
@@ -1,27 +1,30 @@
-console.log('Variables Workshop');
+console.log("Variables Workshop");
 // Q1
-const a = 1
-console.log('Q1: ',a);
+const a = 1;
+console.log("Q1: ", a);
 // Q2
-const b = 'hello'
-console.log('Q2: ',b);
+const b = "hello";
+console.log("Q2: ", b);
 // Q3
-const c = true
-console.log('Q3: ',c);
+const c = true;
+console.log("Q3: ", c);
 // Q4
-const d = 10
-console.log('Q4: ',d);
+const d = 10;
+console.log("Q4: ", d);
 // Q5
-let e = 10
-console.log('Q5.1: ',e);
-e = 20
-console.log('Q5.2: ',e);
+let e = 10;
+console.log("Q5.1 : ", e);
+e = 20;
+console.log("Q5.2 : ", e);
 // Q6
-const myArray = [1,2,3]
-console.log('Q6: ',myArray);
+const myArray = [1, 2, 3];
+console.log("Q6: ", myArray);
 // Q7
-const myObject = {x:1, y:2}
-console.log('Q7: ',myObject);
+const myObject = { x: 1, y: 2 };
+console.log("Q7: ", myObject);
 // Q8
-const myObjects = [{x:1, y:2},{x:3, y:4}]
-console.log('Q8: ',myObjects);
+const myObjects = [
+	{ x: 1, y: 2 },
+	{ x: 3, y: 4 },
+];
+console.log("Q8: ", myObjects);
