2856a9e011f1d8dc37111b4649c5311b4238c5cd
Poom <poomhan@gmail.com>
2023-01-10 10:06:41 +0700

    edit playground

--- playground.js
+++ playground.js
@@ -5,9 +5,12 @@ const c = true;
 const d = 10;
 let e = 10;
 e = 20;
-const myArray = [1,2,3]
-const myObject = [{x:1, y:2}, {x:3,y:4}]
+const myArray = [1, 2, 3];
+const myObject = [
+  { x: 1, y: 2 },
+  { x: 3, y: 4 },
+];
 
-console.log(a, b, c,d,e)
-console.log(Array)
+console.log(a, b, c, d, e);
+console.log(myArray);
 console.log(myObject);
