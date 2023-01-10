342cb76c4852daa6712500eaca4e3f3c92ffdf0b
mplug <oppoalien@gmail.com>
2023-01-10 10:24:02 +0700

    feat: scoping no.1

--- playground.js
+++ playground.js
@@ -1,29 +1,36 @@
-console.log("Hello World");
+// console.log("Hello World");
 
-const a = 1;
-console.log(`1: ${a}`);
+// const a = 1;
+// console.log(`1: ${a}`);
 
-const b = "hello";
-console.log(`2: ${b}`);
+// const b = "hello";
+// console.log(`2: ${b}`);
 
-const c = true;
-console.log(`3: ${c}`);
+// const c = true;
+// console.log(`3: ${c}`);
 
-const d = 10;
-console.log(`4: ${d}`);
+// const d = 10;
+// console.log(`4: ${d}`);
 
-let e = 10;
-e = 20;
-console.log(`5: ${e}`);
+// let e = 10;
+// e = 20;
+// console.log(`5: ${e}`);
 
-const myArray = [1, 2, 3];
-console.log(`6: ${myArray}`);
+// const myArray = [1, 2, 3];
+// console.log(`6: ${myArray}`);
 
-const myObject = { x: 1, y: 2 };
-console.log(`7: ${myObject}`);
+// const myObject = { x: 1, y: 2 };
+// console.log(`7: ${myObject}`);
 
-const myObjects = [
-    { x: 1, y: 2 },
-    { x: 3, y: 4 },
-];
-console.log(`8: ${myObjects}`);
+// const myObjects = [
+//     { x: 1, y: 2 },
+//     { x: 3, y: 4 },
+// ];
+// console.log(`8: ${myObjects}`);
+
+//Scope Workshop
+const a = "hello";
+function greet() {
+    console.log(a);
+}
+greet();
