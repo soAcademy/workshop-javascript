9402d418f4158ef05bfffd85c9cf9f603c473ebc
BornDhamachart <dhamachart.c@gmail.com>
2023-01-10 09:38:50 +0700

    update2

--- playground.js
+++ playground.js
@@ -1,18 +1,21 @@
 const a = 1;
-console.log("a :",a);
+console.log("a :", a);
 const b = "hello";
-console.log("b :",b);
+console.log("b :", b);
 const c = true;
-console.log("c :",c);
+console.log("c :", c);
 const d = 10;
-console.log("d :",d);
+console.log("d :", d);
 let e = 10;
-console.log("e :",e);
+console.log("e :", e);
 e = 20;
-console.log("e :",e);
+console.log("e :", e);
 const myArray = [1, 2, 3];
-console.log("myArray :",myArray);
+console.log("myArray :", myArray);
 const myObject = { x: 1, y: 2 };
-console.log("myObject :",myObject);
-const myObjects = [{ x: 1, y: 2 }, { x: 3, y: 4 }];
-console.log("myObject :",myObject);
+console.log("myObject :", myObject);
+const myObjects = [
+  { x: 1, y: 2 },
+  { x: 3, y: 4 },
+];
+console.log("myObject :", myObject);
