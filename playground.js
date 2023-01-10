711217cb73d341d7a78815089b0b3156cade27a9
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-01-10 11:24:41 +0700

    ans: done function workshop 4

--- playground.js
+++ playground.js
@@ -45,8 +45,13 @@
 
 // console.log(sum2(1, 2, 3));
 
-const sum3 = (a, b, c) => {
-  return { x: a.x + b.x + c.x, y: a.y + b.y + c.y };
-};
+// const sum3 = (a, b, c) => {
+//   return { x: a.x + b.x + c.x, y: a.y + b.y + c.y };
+// };
 
-sum3({ x: 1, y: 1 }, { x: 2, y: 2 }, { x: 3, y: 3 });
+// sum3({ x: 1, y: 1 }, { x: 2, y: 2 }, { x: 3, y: 3 });
+
+const isOdd = (num) => {
+  return num % 2 !== 0;
+};
+console.log(isOdd(7));
