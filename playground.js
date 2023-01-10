88d62e3abd2e3caf57a34df2c9eac88aa134fc6e
korana <korayut@gmail.com>
2023-01-10 09:32:41 +0700

    initial variable

--- playground.js
+++ playground.js
@@ -1,1 +1,11 @@
 console.log("Hello World");
+const a = 1
+const b = 2
+const c = true
+const d = 10
+let e = 10
+e = 20
+const myArry = [1,2,3]
+const myObject = {x:1,y:2}
+const myObject2 = {x:1, y:2}
+const myObject3 = [{x:1,y:2},{x:3, y:4}]
