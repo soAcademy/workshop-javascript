641e068103c1e0c8f3766f5dd0a5c9d65a99d92a
Bond Rungrot <st122150@ait.asia>
2023-01-10 09:56:29 +0700

    init

--- playground.js
+++ playground.js
@@ -2,7 +2,11 @@ const a = 1
 const b = "hello world!"
 const c = true
 const d = 10
-const e = 10
+let e = 10
 e = 20
 const myArray = [1,2,3]
 const myObject = [{x:1, y:2}, {x:3,y:4}]
+
+console.log(a, b, c, d, e)
+console.log(myArray)
+console.log(myObject)
