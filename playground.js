13d7281da80d40e71484d4ae54c24265df7584ed
Your name01~ <your@email.com>
2023-01-10 10:47:30 +0700

    edit3

--- playground.js
+++ playground.js
@@ -1,1 +1,35 @@
 console.log("Hello World");
+console.log("hallo world");
+let a = 1;
+let b = "hallo";
+let c = true;
+const d = 10;
+let e = 10;
+e = 20;
+const myArray = [1, 2, 3];
+const myObject = {x: 1,y: 2};
+const myObjects = [{ x: 1, y: 2 },{ x: 3, y: 4 } ];
+
+console.log(a, b, c, d, e);
+console.log(myArray);
+console.log(myObject);
+
+const a1 = "hello";
+function greet () {
+console.log(a1);
+}
+greet();
+
+const a2 = "hello";
+function greet () {
+a2 = "hi";
+}
+console.log(a2);
+
+const a3 = 10;
+function greet() {
+const b3 = 20;
+
+}
+greet();
+console.log(a + b);
