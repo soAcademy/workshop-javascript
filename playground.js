f1beef1fb807673a9694132687d8faa9d531e419
Your name01~ <your@email.com>
2023-01-10 14:09:33 +0700

    edit3

--- playground.js
+++ playground.js
@@ -1,5 +1,6 @@
-console.log("Hello World");
-console.log("hallo world");
+//console.log("Hello World");
+//console.log("hallo world");
+/*
 let a = 1;
 let b = "hallo";
 let c = true;
@@ -9,6 +10,7 @@ e = 20;
 const myArray = [1, 2, 3];
 const myObject = {x: 1,y: 2};
 const myObjects = [{ x: 1, y: 2 },{ x: 3, y: 4 } ];
+
 
 console.log(a, b, c, d, e);
 console.log(myArray);
@@ -33,3 +35,81 @@ const b3 = 20;
 }
 greet();
 console.log(a + b);
+/** */
+/** 
+function sum1(a, b, c) {
+    let result = a + b +c ;
+    console.log(a, b, c);
+    return result;
+    }
+    console.log(sum1(1,2,3));
+
+
+    
+    
+    const sum2 = (a, b, c) => {
+         const result = a + b +c ;
+         return result
+        //return a + b + c;
+
+        }
+        console.log(sum2(4,5,6));
+
+
+
+        const sum3 = (a, b, c) => {
+        const result = a.x + b.x + c.x, y: a.y + b.y + c.y ;
+    return result ;
+    } 
+    console.log(({x: 1,y: 1},{x: 2,y: 2}));
+
+
+
+const isOdd = (num) => {
+    return num % 2 !== 0;
+
+    };
+//** */
+
+const calculateBMI = (weight, height) => {
+    const bmi = (weight / (height / 100) ** 2).toFixed(2);
+    let score ;
+    if (bmi < 18.5) {
+    grade = 'underweight';
+    } else  
+    if (bmi < 25) {
+    grade = 'healthy';
+    } else 
+    if (bmi < 30) {
+    grade = 'overweight';
+    } else {
+    grade = 'obesity';
+    }
+    console.log(`Your BMI grade is: ${score}`);
+    }; 
+    console.log(calculateBMI(70, 175));
+
+
+    const average = (input) => {
+
+        let sum = 0;
+    
+        for (let i = 0; i < input.length; i++) {
+        sum += input[i];
+        }
+        return sum / input.length;
+        }; // average([1, 2, 3, 4]) => 2.5  
+
+
+        const sumOdd = (input) => {
+
+            let sum = 0;
+            for (let i = 0; i < input.length; i++) {
+            if (input[i] % 2 !== 0) {
+            sum = sum + input[i];
+            }
+             }
+            return sum; };
+
+            console.log(sumOdd([1, 8, 9, -3, 0, 4]));
+            
