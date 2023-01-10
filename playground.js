331d56f66c1d4a09fd40895d2af7a24d373f04c0
korana <korayut@gmail.com>
2023-01-10 09:39:18 +0700

    add console.log

--- playground.js
+++ playground.js
@@ -1,11 +1,23 @@
 console.log("Hello World");
-const a = 1
-const b = 2
-const c = true
-const d = 10
-let e = 10
-e = 20
-const myArry = [1,2,3]
-const myObject = {x:1,y:2}
-const myObject2 = {x:1, y:2}
-const myObject3 = [{x:1,y:2},{x:3, y:4}]
+const a = 1;
+const b = 2;
+const c = true;
+const d = 10;
+let e = 10;
+e = 20;
+const myArry = [1, 2, 3];
+const myObject = { x: 1, y: 2 };
+const myObject2 = { x: 1, y: 2 };
+const myObject3 = [
+  { x: 1, y: 2 },
+  { x: 3, y: 4 },
+];
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(myArr);
+console.log(myObject);
+console.log(myObject2);
+console.log(myObject3);
