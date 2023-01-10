cf188bfef846fc4ac0b2a846f578860d0ef8e7c3
Thanapon Bunchot <116933922+ThanaponBunchot@users.noreply.github.com>
2023-01-10 10:03:33 +0700

    feat: js workshop1

--- playground.js
+++ playground.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+const a = 1;
+console.log("No1 :", a);
+
+const b = "hello"
+console.log("No2 :", b);
+
+const c = true;
+console.log("No3 :", c);
+
+const d = 10;
+console.log("No4 :", d);
+
+let e = 10;
+e = 20;
+console.log("No5 :", e);
+
+const myArray = [1,2,3]
+console.log("No6 :", myArray);
+
+const myObject = {x:1,y:2}
+console.log("No7 :", myObject);
+
+const myObjects = [{x:1,y:2}, {x:3,y:4}]
+console.log("No8 :", myObjects);
