0a7ae59df548a1620d3f296df84ecb16da589504
jaiieth <midzty.rb128@gmail.com>
2023-01-10 09:30:32 +0700

    Variable Workshop

--- playground.js
+++ playground.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+console.log('Variables Workshop');
+// Q1
+const a = 1
+console.log('Q1: ',a);
+// Q2
+const b = 'hello'
+console.log('Q2: ',b);
+// Q3
+const c = true
+console.log('Q3: ',c);
+// Q4
+const d = 10
+console.log('Q4: ',d);
+// Q5
+let e = 10
+console.log('Q5.1: ',e);
+e = 20
+console.log('Q5.2: ',e);
+// Q6
+const myArray = [1,2,3]
+console.log('Q6: ',myArray);
+// Q7
+const myObject = {x:1, y:2}
+console.log('Q7: ',myObject);
+// Q8
+const myObjects = [{x:1, y:2},{x:3, y:4}]
+console.log('Q8: ',myObjects);
