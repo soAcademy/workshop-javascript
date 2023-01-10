eaf6c3f2dd3401236bae423ba7273b4913b84c2b
Your name <your@email.com>
2023-01-10 14:02:32 +0700

    add FLow Control Workshop

--- playground.js
+++ playground.js
@@ -59,3 +59,43 @@ console.log(sum1(20, 30, 40));
 console.log(sum2(20, 30, 40));
 console.log(sum3({ x: 20, y: 20 }, { x: 30, y: 30 }, { x: 40, y: 40 }));
 console.log(isOdd(20));
+
+//Flow Control Workshop 
+const calculateBMI = (weight, height) => {
+  const bmi = weight / (height / 100) ** 2;
+  let grade = "";
+  if (bmi < 18.5) {
+    grade = "underwight";
+  } else if (18.5 <= bmi < 25) {
+    grade = "healthy";
+  } else if (25 <= bmi < 30) {
+    grade = "overweight";
+  } else {
+    grade = "obesity";
+  }
+  return grade;
+};
+
+console.log(calculateBMI(55, 55));
+
+const average = (input) => {
+  let sum = 0;
+  let average = 0;
+  for (let i = 0; i < input.length; i++) {
+    sum += input[i];
+  }
+  average = sum / input.length;
+  return average;
+};
+
+console.log(average([1, 2, 3, 4]));
+
+const sumOdd = (input) => {
+  let sum = 0;
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] % 2 !== 0) sum += input[i];
+  }
+  return average;
+};
+
+console.log(sumOdd([1, 2, 3, 4]));
