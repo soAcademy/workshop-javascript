e643cd4ed189f836131a934978579bb783267c96
Your name <your@email.com>
2023-01-10 10:08:38 +0700

    add playground

--- playground.js
+++ playground.js
@@ -2,11 +2,15 @@ let a = 1;
 let b = "hello";
 let c = true;
 const d = 10;
-const e = 10;
+let e = 10;
 e = 20;
-const myarray = [1, 2, 3];
+const myArray = [1, 2, 3];
 const myObject = { x: 1, y: 2 };
 const myObjects = [
   { x: 1, y: 2 },
   { x: 3, y: 4 },
-];
+]
+
+console.log(a, b, c, d, e);
+console.log(myArray);
+console.log(myObjects);
