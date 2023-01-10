8a10565e01942a32955404f43b7884e8d775a5a5
kunlanis <yoo.kunlanis@gmail.com>
2023-01-10 11:17:20 +0700

    feat:try function workshop topic2

--- playground.js
+++ playground.js
@@ -41,8 +41,13 @@
 // ต้องย้าย console.log ไว้ใน function
 
 //function Workshop
-function sum1(a, b, c) {
+// function sum1(a, b, c) {
+//     return a + b + c;
+//     }
+//     console.log(sum1(1,2,3));
+
+const sum2 = (a, b, c) => {
     return a + b + c;
     }
-    console.log(sum1(1,2,3));
+    console.log(sum2(1,2,3));
 
