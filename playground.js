9294ebee0e300eac58465ace46f75cb09e40cd7d
korana <korayut@gmail.com>
2023-01-10 11:17:04 +0700

    Start Function Workshop

--- playground.js
+++ playground.js
@@ -1,7 +1,26 @@
-let a = 10;
-function greet() {
-    const b = 20;
-    console.log(a+b);
+// const a ="hello";
+// function greet(){
+//     console.log(a);
+// }
+// greet();
 
+// let a = "hello world"
+// function greet(){
+//     a = "hi"
+//     console.log(a);
+// }
+// greet();
+
+// let a = 10;
+// function greet() {
+//     const b = 20;
+//     console.log(a+b);
+
+// }
+// greet();
+
+function sum1(a,b,c) {
+let total = a + b + c;
+console.log(total);
 }
-greet();
+sum1(1,2,3);
