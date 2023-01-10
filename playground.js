4071fef726cb6201e2c9dcbf975cac7e013b7841
kunlanis <yoo.kunlanis@gmail.com>
2023-01-10 11:27:51 +0700

    feat:try function workshop topic3

--- playground.js
+++ playground.js
@@ -46,8 +46,13 @@
 //     }
 //     console.log(sum1(1,2,3));
 
-const sum2 = (a, b, c) => {
-    return a + b + c;
-    }
-    console.log(sum2(1,2,3));
+// const sum2 = (a, b, c) => {
+//     return a + b + c;
+//     }
+//     console.log(sum2(1,2,3));
 
+const sum3 = (a, b, c) => {
+  return { x: a.x + b.x + c.x, y: a.y + b.y + c.y };
+};
+
+console.log(sum3({ x: 1, y: 1 }, { x: 2, y: 2 }, { x: 3, y: 3 }));
