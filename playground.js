8b6c05e2bd77f35f6326b246c4b088d0fd8682a9
kunlanis <yoo.kunlanis@gmail.com>
2023-01-10 10:27:56 +0700

    feat: try scope workshoip topic1

--- playground.js
+++ playground.js
@@ -1,21 +1,37 @@
-console.log("Hello World");
+//console.log("Hello World");
 //1
-const a = 1;
+//const a = 1;
 //2
-const b = "Hello";
+//const b = "Hello";
 //3
-const c = true;
+//const c = true;
 //4
-const d = 10;
+//const d = 10;
 //5
-let e = 10;
-e = 20;
+//let e = 10;
+//e = 20;
 //6
-const myArray = [1, 2, 3];
+//const myArray = [1, 2, 3];
 //7
-const myObject = { x: 1, y: 2 };
+//const myObject = { x: 1, y: 2 };
 //8
-const myObjects = [
-  { x: 1, y: 2 },
-  { x: 3, y: 4 },
-];
+// const myObjects = [
+//   { x: 1, y: 2 },
+//   { x: 3, y: 4 },
+// ];
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(myArray);
+// console.log(myObject);
+// console.log(myObjects);
+
+//Scope Workshop
+const a = "hello";
+function greet(){
+    console.log(a);
+}
+greet();
