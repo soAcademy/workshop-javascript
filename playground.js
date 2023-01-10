fce84c004259d2491658964d573075db7d28892f
Poom <poomhan@gmail.com>
2023-01-10 11:28:57 +0700

    workshop3

--- playground.js
+++ playground.js
@@ -16,7 +16,7 @@ console.log(myArray);
 console.log(myObject); */
 
 
-const a1 = "hello";
+/* const a1 = "hello";
 function greet() {
   console.log(a1);
 }
@@ -33,5 +33,42 @@ function greet3() {
   const b = 20;
   console.log(a + b);
 }
-greet3();
+greet3(); */
+
+/* function sum1(a,b,c) {
+    const d = (a+b+c);
+    return d;
+}
+console.log(sum1(1,2,3)) */
+
+/* const sum2 = (a, b, c) =>{
+  const d = a + b + c;
+  return d;
+}
+console.log(sum2(1, 2, 3)); */
+
+/* const sum3 = (a, b, c) => {
+  return { x: a.x + b.x + c.x, y: a.y + b.y + c.y };
+};
+console.log(sum3({ x: 1, y: 1 }, { x: 2, y: 2 }, { x: 3, y: 3 })); */
+
+const isOdd = (x) => {
+    return x % 2 !== 0;
+}
+console.log(isOdd(2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
