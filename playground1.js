f72a1dd5cc184b4cba8baf358afe4b4a18a91a52
Phisit Siprasatthong <phisite@gmail.com>
2023-01-11 10:59:34 +0700

    feat: add JS func w/s #5 and #6

--- playground1.js
+++ playground1.js
@@ -31,3 +31,22 @@ const array4 = [
 const array4Filtered = array4.filter((x) => x.tel === "0991112222");
 
 console.log(array4Filtered);
+
+console.log("JS Functions Workshop #5");
+
+const array5 = [3, 1, 2, 0];
+const array5Sorted = [...array5]; // shallow copy
+array5Sorted.sort((a, b) => a - b);
+
+console.log(array5Sorted);
+
+console.log("JS Functions Workshop #6");
+
+const array6 = [
+  { id: 2, name: "B" },
+  { id: 1, name: "A" },
+];
+const array6Sorted = [...array6];
+array6Sorted.sort((a, b) => a.id - b.id);
+
+console.log(array6Sorted);
