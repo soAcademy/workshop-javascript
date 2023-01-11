74a39f47009a130f920e5a666b4256144035a298
Firstweera <firstweera357@gmail.com>
2023-01-11 11:03:51 +0700

    js function workshop 2

--- playground1.js
+++ playground1.js
@@ -1,6 +1,21 @@
-const q1 = (input) => {
-  const arrSquare = input.map((r) => r ** 2);
-  return arrSquare;
+// Q1
+
+// const q1 = (input) => {
+//   const arrSquare = input.map((r) => r ** 2);
+//   return arrSquare;
+// };
+
+// console.log(q1([1, 2, 3, 4]));
+
+// Q2
+let customer = [
+  { id: 1, name: "A" },
+  { id: 2, name: "B" },
+];
+
+const q2 = (input) => {
+  const names = input.map((r) => r.name);
+  return names;
 };
 
-console.log(q1([1, 2, 3, 4]));
+console.log(q2(customer));
