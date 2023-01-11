3879fbd279e8cef3d48c09ac85cba88d250f0cd4
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-01-11 11:00:09 +0700

    ans: function workshop 4

--- playground1.js
+++ playground1.js
@@ -21,11 +21,24 @@
 
 // ############ ข้อ 2 ###############
 
-const array = [1, 2, 3, 4, 5];
+// const array = [1, 2, 3, 4, 5];
 
-const oddNum = (num) => {
-  const odd = num.filter((r) => r % 2 !== 0);
-  return odd;
+// const oddNum = (num) => {
+//   const odd = num.filter((r) => r % 2 !== 0);
+//   return odd;
+// };
+
+// console.log(oddNum(array));
+
+// ############ ข้อ 3 ###############
+
+const userInfo = [
+  { id: 1, name: "A", tel: "0818218888" },
+  { id: 2, name: "B", tel: "099111222" },
+];
+
+const filterUser = (info, phone) => {
+  const filterInfo = info.filter((r) => r.tel === phone);
+  return filterInfo;
 };
-
-console.log(oddNum(array));
+console.log(filterUser(userInfo, "099111222"));
