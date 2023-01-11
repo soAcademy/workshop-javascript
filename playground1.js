bb60a711917ad64f65860ecfabf372b6a31e632e
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-01-11 10:51:28 +0700

    ans: function workshop 3

--- playground1.js
+++ playground1.js
@@ -7,14 +7,25 @@
 
 // ############ ข้อ 1 ###############
 
-const user = [
-  { id: 1, name: "A" },
-  { id: 2, name: "B" },
-];
+// const user = [
+//   { id: 1, name: "A" },
+//   { id: 2, name: "B" },
+// ];
 
-const userNames = (obj) => {
-  const names = obj.map((r) => r.name);
-  return names;
+// const userNames = (obj) => {
+//   const names = obj.map((r) => r.name);
+//   return names;
+// };
+
+// console.log(userNames(user));
+
+// ############ ข้อ 2 ###############
+
+const array = [1, 2, 3, 4, 5];
+
+const oddNum = (num) => {
+  const odd = num.filter((r) => r % 2 !== 0);
+  return odd;
 };
 
-console.log(userNames(user));
+console.log(oddNum(array));
