ee41dade747bd2f9ba3993c5b7f38bd27aafa2e3
Firstweera <firstweera357@gmail.com>
2023-01-11 11:24:57 +0700

    js function workshop 4

--- playground1.js
+++ playground1.js
@@ -23,9 +23,23 @@
 
 // Q3
 
-const q3 = (input) => {
-  const oddNumber = input.filter((r) => r % 2 !== 0);
-  return oddNumber;
+// const q3 = (input) => {
+//   const oddNumber = input.filter((r) => r % 2 !== 0);
+//   return oddNumber;
+// };
+
+// console.log(q3([1, 2, 3, 4, 5]));
+
+// Q4
+
+let customer = [
+  { id: 1, name: "A", tel: "0818218888" },
+  { id: 2, name: "B", tel: "0991112222" },
+];
+
+const q4 = (input, phoneNumber) => {
+  const findPhoneNumber = input.filter((r) => r.tel === phoneNumber);
+  return findPhoneNumber;
 };
 
-console.log(q3([1, 2, 3, 4, 5]));
+console.log(q4(customer, "0991112222"));
