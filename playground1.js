27a31cfdcc9c6009cdb9748b06b77dc9befd6235
Firstweera <firstweera357@gmail.com>
2023-01-11 11:08:23 +0700

    js function workshop 3

--- playground1.js
+++ playground1.js
@@ -8,14 +8,24 @@
 // console.log(q1([1, 2, 3, 4]));
 
 // Q2
-let customer = [
-  { id: 1, name: "A" },
-  { id: 2, name: "B" },
-];
 
-const q2 = (input) => {
-  const names = input.map((r) => r.name);
-  return names;
+// let customer = [
+//   { id: 1, name: "A" },
+//   { id: 2, name: "B" },
+// ];
+
+// const q2 = (input) => {
+//   const names = input.map((r) => r.name);
+//   return names;
+// };
+
+// console.log(q2(customer));
+
+// Q3
+
+const q3 = (input) => {
+  const oddNumber = input.filter((r) => r % 2 !== 0);
+  return oddNumber;
 };
 
-console.log(q2(customer));
+console.log(q3([1, 2, 3, 4, 5]));
