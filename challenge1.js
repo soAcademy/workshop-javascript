a846b33ec4afe05a1f7c090e305ea3a7cd203104
Phisit Siprasatthong <phisite@gmail.com>
2023-01-11 13:59:37 +0700

    feat: add new challenge files and remove old file

---
+++ challenge1.js
@@ -0,0 +1,87 @@
+const products = [
+  {
+    id: 1,
+    price: 9.99,
+    cost: 7.25,
+    quantity: 50,
+    name: "Product 1",
+  },
+  {
+    id: 2,
+    price: 19.99,
+    cost: 14.5,
+    quantity: 40,
+    name: "Product 2",
+  },
+  {
+    id: 3,
+    price: 29.99,
+    cost: 21.35,
+    quantity: 30,
+    name: "Product 3",
+  },
+];
+
+const orders = [
+  {
+    id: 1,
+    customer: "John Doe",
+    items: [
+      {
+        productId: 1,
+        quantity: 2,
+      },
+      {
+        productId: 2,
+        quantity: 1,
+      },
+    ],
+    datetime: "2022-12-15T12:00:00Z",
+  },
+  {
+    id: 2,
+    customer: "Jane Smith",
+    items: [
+      {
+        productId: 3,
+        quantity: 3,
+      },
+    ],
+    datetime: "2022-12-14T12:00:00Z",
+  },
+  {
+    id: 3,
+    customer: "Bob Johnson",
+    items: [
+      {
+        productId: 1,
+        quantity: 1,
+      },
+    ],
+    datetime: "2022-12-13T12:00:00Z",
+  },
+  {
+    id: 4,
+    customer: "John Doe",
+    items: [
+      {
+        productId: 2,
+        quantity: 2,
+      },
+      {
+        productId: 3,
+        quantity: 1,
+      },
+    ],
+    datetime: "2022-12-13T11:00:00Z",
+  },
+];
+
+// Quiz: 1
+
+const calculateTotalSale = () => {};
+console.log();
+
+// Quiz: 2
+const findTopValueCustomer = () => {};
+console.log;
---
+++ challenge2.js
@@ -0,0 +1,107 @@
+const users = [
+  {
+    userId: 1,
+    name: "Bob",
+  },
+  {
+    userId: 2,
+    name: "Bin",
+  },
+  {
+    userId: 3,
+    name: "Jam",
+  },
+  {
+    userId: 4,
+    name: "Silvy",
+  },
+  {
+    userId: 5,
+    name: "Ploy",
+  },
+];
+
+const groups = [
+  {
+    groudId: "g1",
+    name: "Group1",
+    users: [
+      {
+        userId: 1,
+        userId: 2,
+      },
+    ],
+  },
+  {
+    groudId: "g2",
+    name: "Group2",
+    users: [
+      {
+        userId: 3,
+        userId: 4,
+        userId: 5,
+      },
+    ],
+  },
+  {
+    groudId: "g3",
+    name: "Group3",
+    users: [
+      {
+        userId: 0,
+      },
+    ],
+  },
+];
+
+const documents = [
+  {
+    documentId: "d1",
+    permissions: [
+      {
+        userId: 1,
+        role: "ADMIN",
+      },
+      {
+        groupId: "g1",
+        role: "EDITOR",
+      },
+    ],
+  },
+  {
+    documentId: "d2",
+    permissions: [
+      {
+        userId: 3,
+        role: "EDITOR",
+      },
+      {
+        groupId: "g1",
+        role: "EDITOR",
+      },
+      {
+        groupId: "g2",
+        role: "EDITOR",
+      },
+    ],
+  },
+  {
+    documentId: "d3",
+    permissions: [
+      {
+        groupId: "g2",
+        role: "EDITOR",
+      },
+    ],
+  },
+];
+
+// Quiz
+// 1. สร้างฟังก์ชั่น hasDocumentPermission({documentId, name, role}) แล้ว return ว่ามี permissions หรือไม่
+// เช่น hasDocumentPermissioin({documentId: 'd3', name: 'Jam', role: 'EDITOR'}) -> true
+
+const hasDocumentPermission = ({ documentId, name, role }) => {};
+
+console.log(
+  hasDocumentPermission({ documentId: "d3", name: "Jam", role: "EDITOR" })
+);
