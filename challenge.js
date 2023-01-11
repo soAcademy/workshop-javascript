c2d7f6d75edcf723b11f0d0026d0f8d6f410826b
Phisit Siprasatthong <phisite@gmail.com>
2023-01-11 13:34:37 +0700

    feat: add challenge w/ initial values

---
+++ challenge.js
@@ -0,0 +1,47 @@
+const products = [
+  {
+    id: 1,
+    price: 9.99,
+    cost: 7.25,
+    quantity: 50,
+    name: "Product 1",
+  },
+  {
+    id: 2,
+    price: 19.99,
+    cost: 14.5,
+    quantity: 40,
+    name: "Product 2",
+  },
+  {
+    id: 3,
+    price: 29.99,
+    cost: 21.35,
+    quantity: 30,
+    name: "Product 3",
+  },
+];
+
+const orders = [
+  {
+    id: 1,
+    price: 9.99,
+    cost: 7.25,
+    quantity: 50,
+    name: "Product 1",
+  },
+  {
+    id: 2,
+    price: 19.99,
+    cost: 14.5,
+    quantity: 40,
+    name: "Product 2",
+  },
+  {
+    id: 3,
+    price: 29.99,
+    cost: 21.35,
+    quantity: 30,
+    name: "Product 3",
+  },
+];
