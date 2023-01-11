49e6ea2558b493fb13cd6a6602e27b4a5dee0ec3
Firstweera <firstweera357@gmail.com>
2023-01-11 11:29:41 +0700

    js function workshop 5

--- playground1.js
+++ playground1.js
@@ -32,14 +32,23 @@
 
 // Q4
 
-let customer = [
-  { id: 1, name: "A", tel: "0818218888" },
-  { id: 2, name: "B", tel: "0991112222" },
-];
+// let customer = [
+//   { id: 1, name: "A", tel: "0818218888" },
+//   { id: 2, name: "B", tel: "0991112222" },
+// ];
 
-const q4 = (input, phoneNumber) => {
-  const findPhoneNumber = input.filter((r) => r.tel === phoneNumber);
-  return findPhoneNumber;
+// const q4 = (input, phoneNumber) => {
+//   const findPhoneNumber = input.filter((r) => r.tel === phoneNumber);
+//   return findPhoneNumber;
+// };
+
+// console.log(q4(customer, "0991112222"));
+
+// Q5
+
+const q6 = (input) => {
+  input.sort();
+  return input;
 };
 
-console.log(q4(customer, "0991112222"));
+console.log(q6([3, 1, 2, 0]));
