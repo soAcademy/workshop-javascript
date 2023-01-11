b492c672de499919940368bfba88a3bc1764ac48
Panasun Sunanta <panasunsunanta@gmail.com>
2023-01-11 16:00:13 +0700

    feat: additional quiz

--- challenge2.js
+++ challenge2.js
@@ -20,7 +20,7 @@ const users = [
     name: "Ploy",
   },
 ];
- 
+
 const groups = [
   {
     groudId: "g1",
@@ -28,6 +28,8 @@ const groups = [
     users: [
       {
         userId: 1,
+      },
+      {
         userId: 2,
       },
     ],
@@ -38,7 +40,11 @@ const groups = [
     users: [
       {
         userId: 3,
+      },
+      {
         userId: 4,
+      },
+      {
         userId: 5,
       },
     ],
@@ -53,54 +59,74 @@ const groups = [
     ],
   },
 ];
- 
+
+// const groups2 = [
+//   {
+//     groudId: "g1",
+//     name: "Group1",
+//     users: [
+//       {
+//         userId: 1,
+//         name: "Bob",
+//       },
+//       {
+//         userId: 2,
+//         name: "Bin",
+//       },
+//     ],
+//   },
+// ];
+
 const documents = [
   {
-    documentId: 'd1',
+    documentId: "d1",
     permissions: [
       {
         userId: 1,
-        role: 'ADMIN'
+        role: "ADMIN",
       },
       {
         groupId: "g1",
-        role: 'EDITOR'
-      }
-    ]
+        role: "EDITOR",
+      },
+    ],
   },
   {
-    documentId: 'd2',
+    documentId: "d2",
     permissions: [
       {
         userId: 3,
-        role: 'EDITOR'
+        role: "EDITOR",
       },
       {
         groupId: "g1",
-        role: 'EDITOR'
+        role: "EDITOR",
       },
       {
         groupId: "g2",
-        role: 'EDITOR'
-      }
-    ]
+        role: "EDITOR",
+      },
+    ],
   },
   {
-    documentId: 'd3',
+    documentId: "d3",
     permissions: [
       {
         groupId: "g2",
-        role: 'EDITOR'
-      }
-    ]
-  }
-]
+        role: "EDITOR",
+      },
+    ],
+  },
+];
 
 // Quiz
 // 1. สร้างฟังก์ชั่น hasDocumentPermission({documentId, name, role}) แล้ว return ว่ามี permissions หรือไม่
 // เช่น hasDocumentPermissioin({documentId: 'd3', name: 'Jam', role: 'EDITOR'}) -> true
 // .some, .filter
+// 2. สร้าง group2 ที่ดึงข้อมูล name มาด้วย
 
-const hasDocumentPermission = ({documentId, name, role}) => {}
+const hasDocumentPermission = ({ documentId, name, role }) => {};
 
-console.log(hasDocumentPermission({documentId: 'd3', name: 'Jam', role: 'EDITOR'}))
+console.log(
+  hasDocumentPermission({ documentId: "d3", name: "Jam", role: "EDITOR" })
+);
