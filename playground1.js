ded91da68ba33813beb221d02b38507105def137
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-01-11 10:43:50 +0700

    ans: function workshop 2

--- playground1.js
+++ playground1.js
@@ -1,6 +1,20 @@
-const powerFunc = (num) => {
-  const result = num.map((r) => r ** 2);
-  return result;
+// const powerFunc = (num) => {
+//   const result = num.map((r) => r ** 2);
+//   return result;
+// };
+
+// console.log(powerFunc([1, 2, 3, 4]));
+
+// ############ ข้อ 1 ###############
+
+const user = [
+  { id: 1, name: "A" },
+  { id: 2, name: "B" },
+];
+
+const userNames = (obj) => {
+  const names = obj.map((r) => r.name);
+  return names;
 };
 
-console.log(powerFunc([1, 2, 3, 4]));
+console.log(userNames(user));
