ea79c27d97c3bb808fedf4cff753f8d96bef015a
jaiieth <midzty.rb128@gmail.com>
2023-01-12 10:12:34 +0700

    feat:add saleByDate()

--- challenge1.js
+++ challenge1.js
@@ -78,17 +78,17 @@ let products = [
 ];
 
 // calculate total sale
-// const calTotalSale = (orders, products) => {
-//   let total = 0;
-//   orders.map((order) =>
-//     order.items.map(
-//       (item) =>
-//         (total +=
-//           item.quantity * products.find((e) => e.id === item.productId).price)
-//     )
-//   );
-//   return total
-// };
+const calTotalSale = (orders, products) => {
+  let total = 0;
+  orders.map((order) =>
+    order.items.map(
+      (item) =>
+        (total +=
+          item.quantity * products.find((e) => e.id === item.productId).price)
+    )
+  );
+  return total
+};
 // console.log(calTotalSale(orders, products));
 
 // Find Best Seller Product
@@ -124,43 +124,82 @@ let products = [
 // console.log(topSellingProduct(orders, products))
 
 // Find Top Buyer
-// {
 //   customer: String,
+// {
+//   saleValue: Number,
 //   saleValue: Number,
 //   products: {
 //     id: String,
 //     name: String,
 //     quantity: String}
 //   }
-const topBuyer = (orders, products) => {
-  let customers = orders.map((e) => e.customer);
-  customers = [...new Set(customers)];
-  let result = customers.map((customer) => {
-    let product = {};
-    return orders
-      .filter((order) => order.customer === customer)
-      .reduce((acc, e) => {
-        let sumValue = 0;
-        e.items.map((i) => {
-          let price = products.find((e) => e.id === i.productId).price;
-          sumValue += i.quantity * price;
-          product[i.productId] = {
-            id: i.productId,
-            name: products.find((e) => e.id === i.productId).name,
-            quantity: (product[i.productId]?.quantity ?? 0) + i.quantity,
-          };
-        });
+// const topBuyer = (orders, products) => {
+//   let customers = orders.map((e) => e.customer);
+//   customers = [...new Set(customers)];
+//   let result = customers.map((customer) => {
+//     let product = {};
+//     return orders
+//       .filter((order) => order.customer === customer)
+//       .reduce((acc, e) => {
+//         let sumValue = 0;
+//         e.items.map((i) => {
+//           let price = products.find((e) => e.id === i.productId).price;
+//           sumValue += i.quantity * price;
+//           product[i.productId] = {
+//             id: i.productId,
+//             name: products.find((e) => e.id === i.productId).name,
+//             quantity: (product[i.productId]?.quantity ?? 0) + i.quantity,
+//           };
+//         });
 
-        acc[e.customer] = {
-          customer: e.customer,
-          saleValue: (acc[e.customer]?.saleValue ?? 0) + sumValue,
-          products: product,
-        };
-        return acc;
-      }, {});
-  });
-  result = result.map((e) => Object.values(e)).flat();
-  result.sort((a, b) => b.saleValue - a.saleValue);
-  return result[0];
-};
-console.log(topBuyer(orders, products));
+//         acc[e.customer] = {
+//           customer: e.customer,
+//           saleValue: (acc[e.customer]?.saleValue ?? 0) + sumValue,
+//           products: product,
+//         };
+//         return acc;
+//       }, {});
+//   });
+//   result = result.map((e) => Object.values(e)).flat();
+//   result.sort((a, b) => b.saleValue - a.saleValue);
+//   return result[0];
+// };
+// console.log(topBuyer(orders, products));
+
+// Summarize sale by date
+// {
+//   date: String, 
+//   saleValue: Number, 
+//   costValue: Number, 
+//   profit: Number, 
+//   saleQuantity: Number
+// }
+
+const saleByDate  = (orders, products) => {
+  const getDate = (date) => date.slice(0,10).split()
+  let saleByDate = {}
+  orders.map(order=>{
+    let sale = 0
+    let cost = 0
+    let quantity = 0
+    date = getDate(order.datetime)
+    order.items.map(item=>{
+      sale += item.quantity * products.find(e=> e.id === item.productId).price
+      cost += item.quantity * products.find(e=> e.id === item.productId).cost
+      quantity += item.quantity
+    })
+    saleByDate[date] = {
+      date: date[0],
+      saleValue: (saleByDate[date]?.saleValue ?? 0) + sale,
+      costValue: (saleByDate[date]?.costValue ?? 0) + cost,
+      profit: (saleByDate[date]?.profit?? 0) + (sale-cost),
+      saleQuantity: (saleByDate[date]?.saleQuantity ?? 0) + quantity
+    }
+    // console.log(date);
+    // console.log('sale: ',sale,'cost: ',cost,'quantity: ',quantity);
+  })
+  saleByDate = Object.values(saleByDate)
+  return saleByDate
+}
+
+console.log(saleByDate(orders, products))
