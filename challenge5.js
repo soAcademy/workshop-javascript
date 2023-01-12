9a65b72f84ed1e831091ca853192ea82c4892780
Panasun Sunanta <panasunsunanta@gmail.com>
2023-01-12 12:09:06 +0700

    feat: challenge 5

---
+++ challenge5.js
@@ -0,0 +1,31 @@
+const csvData = `id,name,orderValue,orderDate
+1,Bin,1340,14/01/2023
+2,Jam,250,14/01/2023
+3,Ploy,350,14/01/2023
+4,Bin,950,15/01/2023
+5,Jam,850,15/01/2023`;
+
+// Quiz 1: convert csvData to object like these
+// [
+//   {
+//     id: 1,
+//     name: 'Bin',
+//     orderValue: 1340,
+//     orderDate: "14/01/2023"
+//   }
+// ]
+
+const convertCsvToObject = (csvData) => {
+  const rows = csvData.split("\n");
+  const fieldName = rows[0].split(",");
+  const datas = rows.splice(1, rows.length - 1);
+  const result = datas.map((data) => {
+    const fieldValues = data.split(",");
+    return fieldName.map((key, idx) => ({
+      [key]: fieldValues[idx],
+    }));
+  });
+  return result;
+};
+
+console.log("Q1: ", convertCsvToObject(csvData));
