e6c2fed8c649cbe7e35c078dfac2d27a01a05cd7
Panasun Sunanta <panasunsunanta@gmail.com>
2023-01-12 09:46:49 +0700

    feat: add q3

---
+++ challenge3.js
@@ -0,0 +1,6 @@
+const customers = [
+  { name: "John", age: 25, hobbies: ["reading", "hiking", "swimming"] },
+  { name: "Jane", age: 32, hobbies: ["cooking", "dancing", "traveling"] },
+  { name: "Bob", age: 28, hobbies: ["cycling", "painting"] },
+  { name: "Alice", age: 22, hobbies: ["reading", "gardening"] }
+]
