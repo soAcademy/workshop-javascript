969c014295020b7ce5668521b802c788eaca6488
Panasun Sunanta <panasunsunanta@gmail.com>
2023-01-12 15:34:46 +0700

    feat: update solution challenge 7

--- challenge7.js
+++ challenge7.js
@@ -1,5 +1,16 @@
-// Q1: Find second max number using .reduce (avoid using forEach, if/else)
-const numbers = [1, 3, 7, 4, 9, 11, 6];
-console.log(findSecondMax(numbers)); // Output: 9
+// Q1: Find second max number using .reduce (avoid using forEach, if/else, sort)
 // Hint: Reduce acc object should intialize with {max: -999999, secondMax: -999999}
 
+const findSecondMax = (numbers) => numbers.reduce((acc, r) => {
+  acc = {
+    max: Math.max(acc.max, r),
+    // secondMax: r >= acc.max ? acc.max : Math.max(acc.secondMax, r)
+    secondMax: Math.max(acc.secondMax, Math.min(acc.max, r))
+
+  }
+  return acc;
+}, {max: -999999, secondMax: -999999}).secondMax
+
+
+const numbers = [1, 3, 7, 4, 9, 11, 6];
+console.log(findSecondMax(numbers)); // Output: 9
