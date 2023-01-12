b69542115e1e568acf03cc006310e048e37c0daf
Panasun Sunanta <panasunsunanta@gmail.com>
2023-01-12 15:57:10 +0700

    feat: add calculate roc

--- challenge7.js
+++ challenge7.js
@@ -13,3 +13,19 @@ const findSecondMax = (numbers) =>
 
 const numbers = [1, 3, 7, 4, 9, 11, 6];
 console.log(findSecondMax(numbers)); // Output: 9
+
+// Q2: Given stock prices calculate rate of return by using .reduce / .map
+const calculateROC = (stockPrices) =>
+  stockPrices
+    .reduce(
+      (acc, price) => ({
+        previousPrice: price,
+        idx: (acc.idx ?? 0) + 1,
+        rocs: [...acc.rocs, acc.idx === 0 || price / acc.previousPrice - 1],
+      }),
+      { previousPrice: 0, idx: 0, rocs: [] }
+    )
+    .rocs.slice(1);
+
+const stockPrices = [100, 105, 103, 99, 95, 98, 101, 104];
+console.log(calculateROC(stockPrices));
