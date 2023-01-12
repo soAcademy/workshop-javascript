9e733ce0a7197677574c5ce9eb50df387375960a
Your name <your@email.com>
2023-01-12 10:03:52 +0700

    update challenge.js

---
+++ challenge.js
@@ -0,0 +1,97 @@
+const products = [
+  {
+    "id": 1,
+    "price": 9.99,
+    "cost": 7.25,
+    "quantity": 50,
+    "name": "Product 1"
+  },
+  {
+    "id": 2,
+    "price": 19.99,
+    "cost": 14.5,
+    "quantity": 40,
+    "name": "Product 2"
+  },
+  {
+    "id": 3,
+    "price": 29.99,
+    "cost": 21.35,
+    "quantity": 30,
+    "name": "Product 3"
+  }
+]
+
+const orders = [
+  {
+    "id": 1,
+    "customer": "John Doe",
+    "items": [
+      {
+        "productId": 1,
+        "quantity": 2
+      },
+      {
+        "productId": 2,
+        "quantity": 1
+      }
+    ],
+    "datetime": "2022-12-15T12:00:00Z"
+  },
+  {
+    "id": 2,
+    "customer": "Jane Smith",
+    "items": [
+      {
+        "productId": 3,
+        "quantity": 3
+      }
+    ],
+    "datetime": "2022-12-14T12:00:00Z"
+  },
+  {
+    "id": 3,
+    "customer": "Bob Johnson",
+    "items": [
+      {
+        "productId": 1,
+        "quantity": 1
+      }
+    ],
+    "datetime": "2022-12-13T12:00:00Z"
+  },
+  {
+    "id": 4,
+    "customer": "John Doe",
+    "items": [
+      {
+        "productId": 2,
+        "quantity": 2
+      },
+      {
+        "productId": 3,
+        "quantity": 1
+      }
+    ],
+    "datetime": "2022-12-13T11:00:00Z"
+  }
+]
+
+//#1
+// const totalIncome = (orders, products) => {
+//   const totalSales = orders.map(order) => {
+//     const items = order.items.map(item) => {
+//       const product = products.find(product => product.id === item.productId);
+     
+//     }
+//   }
+// }
+
+//#1
+const calculateTotalSales = (orders, products) => {
+  const totalSales = orders.map(order) => {
+    const items = order.items.map(item) => {
+      const product = products.find(product => product.id === item.productId);
+    }
+  }
+}
