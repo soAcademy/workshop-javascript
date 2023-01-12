d6ff5d3761fae19aabaf0f2f2131d023e495823e
Panasun Sunanta <panasunsunanta@gmail.com>
2023-01-12 09:51:05 +0700

    feat: update quiz

--- challenge3.js
+++ challenge3.js
@@ -4,3 +4,26 @@ const customers = [
   { name: "Bob", age: 28, hobbies: ["cycling", "painting", "swimming"] },
   { name: "Alice", age: 22, hobbies: ["reading", "gardening"] }
 ]
+
+// Quiz 1: Filter customer age >= 25 and age <= 30 and then get unique hobbies
+// ["reading", "hiking", "swimming", "cycling", "painting"]
+
+// Quiz 2: Find customer that interest in swimming
+// ['Johnn', 'Bob']
+
+// Quiz 3: Count customers by hobby and by count desc
+// [
+//   {
+//     hobby: "reading",
+//     count: 2
+//   },
+//   {
+//     hobby: "swimming",
+//     count: 2
+//   },
+//   {
+//     hobby: "hiking",
+//     count: 1
+//   },
+//   ...
+// ]
