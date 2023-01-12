a364da63698cd80d717cde92d7016d26c6fd677e
kunlanis <yoo.kunlanis@gmail.com>
2023-01-12 13:32:48 +0700

    feat: resolve conflict

--- challenge3.js
+++ challenge3.js
@@ -8,23 +8,36 @@ const customers = [
 // Quiz 1: Filter customer age >= 25 and age <= 30 and then get unique hobbies
 // ["reading", "hiking", "swimming", "cycling", "painting"]
 // .filter, .map, .flat, .unique
+// const filteredAge = customers.filter(c => c.age >= 25 && c.age <= 30);
+// //  console.log(filteredAge);
+// const hobbies = filteredAge.map(item => item.hobbies).flat();
+// console.log("hobbies",hobbies);
+// const uniqueHobbies = [...new Set(hobbies)];
+// console.log("uniqueHobbies",uniqueHobbies);
+const findUniqueHobbies = (customers) => {
+  const hobbies = customers
+  .filter(customer => customer.age >= 25 && customer.age <= 30)
+  .map(customer => customer.hobbies)
+  .flat();
+  const uniqueHobbies = [...new Set(hobbies)];
+  return uniqueHobbies
+}
+console.log("find unique hobbies",findUniqueHobbies(customers));
 
 // Quiz 2: Find customer that interest in swimming
-// ['Johnn', 'Bob']
+// ['John', 'Bob']
+const findSwimCustomer = (customers) => {
+  const swimCustomer = customers
+  .filter(customer => customer.hobbies.some((hobby)=>  hobby === 'swimming'))
+  .map(customer => customer.name);
+  return swimCustomer
+}
+console.log("find swim customer",findSwimCustomer(customers));
 
 // Quiz 3: Count customers by hobby and by count desc
 // [
-//   {
-//     hobby: "reading",
-//     count: 2
-//   },
-//   {
-//     hobby: "swimming",
-//     count: 2
-//   },
-//   {
-//     hobby: "hiking",
-//     count: 1
-//   },
-//   ...
-// ]
+//   {hobby: "reading",count: 2},
+//   {hobby: "swimming",count: 2},
+//   {hobby: "hiking",count: 1},...]
+
+
