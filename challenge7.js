4c01421251d6cf618425e6bbdbf3ea92c8db6ed2
jaiieth <midzty.rb128@gmail.com>
2023-01-13 13:55:19 +0700

    feat: secondMax(), calROC()

--- challenge7.js
+++ challenge7.js
@@ -1,31 +1,39 @@
 // Q1: Find second max number using .reduce (avoid using forEach, if/else, sort)
 // Hint: Reduce acc object should intialize with {max: -999999, secondMax: -999999}
 
-const findSecondMax = (numbers) =>
-  numbers.reduce(
-    (acc, r) => ({
-      max: Math.max(acc.max, r),
-      // secondMax: r >= acc.max ? acc.max : Math.max(acc.secondMax, r)
-      secondMax: Math.max(acc.secondMax, Math.min(acc.max, r)),
-    }),
+const secondMax = (nums) => {
+  const result = nums.reduce(
+    (result, num) => {
+      // compare current secondMax to current num
+      const comparedNum = result.secondMax > num ? result.secondMax : num;
+      // compare current max to comparedNum
+      return {
+        max: result.max > comparedNum ? result.max : comparedNum,
+        secondMax: result.max > comparedNum ? comparedNum : result.max,
+      };
+    },
     { max: -999999, secondMax: -999999 }
-  ).secondMax;
+  );
+  return result.secondMax;
+};
 
-const numbers = [1, 3, 7, 4, 9, 11, 6];
-console.log(findSecondMax(numbers)); // Output: 9
+console.log(secondMax([15, 20, 35, 36, 38, 99, 30, 28]));
 
 // Q2: Given stock prices calculate rate of return by using .reduce / .map
-const calculateROC = (stockPrices) =>
-  stockPrices
-    .reduce(
-      (acc, price) => ({
-        previousPrice: price,
-        idx: (acc.idx ?? 0) + 1,
-        rocs: [...acc.rocs, acc.idx === 0 ? 0 : price / acc.previousPrice - 1],
-      }),
-      { previousPrice: 0, idx: 0, rocs: [] }
-    )
-    .rocs.slice(1);
-
-const stockPrices = [100, 105, 103, 99, 95, 98, 101, 104];
-console.log(calculateROC(stockPrices));
+const stockPrices = [100, 120, 130, 145, 120, 110, 150];
+const calROC = (prices, n) => {
+  return prices.map((price, idx) => {
+    return idx - n >= 0
+      ? {
+          price: price,
+          roc:
+            Math.round((price / prices[idx - n] - 1 + Number.EPSILON) * 1000) /
+            1000,
+        }
+      : {
+          price: price,
+          roc: null,
+        };
+  });
+};
+console.log(calROC(stockPrices, 2));
