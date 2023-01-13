791b729824234732309707e7dd36c45e1654aee8
Thanapon Bunchot <116933922+ThanaponBunchot@users.noreply.github.com>
2023-01-13 21:27:45 +0700

    completed challange4

--- challenge4.js
+++ challenge4.js
@@ -112,22 +112,63 @@ const orders = [
 //   ...
 // ]
 
-const calculateOrderValue = (orders, shippingByOrderValueTiers) =>
-  orders.map((order) => {
-    const orderValue = order.items.reduce(
+// const calculateOrderValue = (orders, shippingByOrderValueTiers) =>
+//   orders.map((order) => {
+//     const orderValue = order.items.reduce(
+//       (acc, r) => acc + r.quantity * r.price,
+//       0
+//     );
+//     const shippingTier = shippingByOrderValueTiers.find(
+//       (shipping) => orderValue <= shipping.orderValueLimit
+//     );
+//     const totalValue = orderValue + shippingTier.shippingPrice;
+
+//     return {
+//       ...order,
+//       orderValue,
+//       shippingPrice: shippingTier.shippingPrice,
+//       totalValue,
+//     };
+//   });
+
+// console.log(calculateOrderValue(orders, shippingByOrderValueTiers));
+
+//  console.log(
+//   JSON.stringify(calculateOrderValue(orders, shippingByOrderValueTiers), null, 4)
+//   );
+
+const findValue = (inputOrder, InputShipping) =>
+  inputOrder.map((data) => {
+    const orderValue = data.items.reduce(
       (acc, r) => acc + r.quantity * r.price,
       0
     );
-    const shippingTier = shippingByOrderValueTiers.find(
+
+    //Find teir
+    const findTier = InputShipping.find(
       (shipping) => orderValue <= shipping.orderValueLimit
     );
-    const totalValue = orderValue + shippingTier.shippingPrice;
+    //sum all
+    const finalPrice = findTier.shippingPrice + orderValue;
+    // console.log(finalPrice);
     return {
-      ...order,
+      customer: data.customer,
+      items: data.items,
       orderValue,
-      shippingPrice: shippingTier.shippingPrice,
-      totalValue,
+      shippingPrice: findTier.shippingPrice,
+      finalPrice,
+      //ปกติถ้าอยากได้ value (ระบุ idx ของ key เ่ช่น orders[0].customer แต่ถ้าใช้วิธี map ระบุแค่ key จะได้หมดเลย)
     };
   });
 
-console.log("Q1: ", calculateOrderValue(orders, shippingByOrderValueTiers));
+console.log(
+  JSON.stringify(findValue(orders, shippingByOrderValueTiers), null, 1)
+);
+// console.log("--------------------------------------------------------");
+
+// const multiply = (input)=>{
+//   const output = input*20;
+//   return output +10;
+// }
+// console.log(multiply(20));
+// console.log(shippingByOrderValueTiers[0].shippingPrice);
