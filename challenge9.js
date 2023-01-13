8b6a5f5cd5d241f8927e1d8426ae347abce9eb56
Phisit Siprasatthong <phisite@gmail.com>
2023-01-13 12:00:59 +0700

    fix: remove solutions from challenge9

--- challenge9.js
+++ challenge9.js
@@ -1,29 +1,13 @@
-// Q1: Calculate fibbonacci using reduce
+// Q1: Calculate Fibonacci using reduce
 // 0 1 1 2 3 5 8 13 21
-const fib = (n) =>
-  [...Array(n).keys()].reduce(
-    (acc, _r) => {
-      const vt = acc.v1;
-      acc.v1 = acc.v1 + acc.v0;
-      acc.v0 = vt;
-      return acc;
-    },
-    { v0: 0, v1: 1 }
-  ).v1;
+const fib = () => {};
 
 // 13
 console.log(fib(6));
 
-// Q2: Calculate fibbonacci series using reduce
+// Q2: Calculate Fibonacci series using reduce
 
-const fibSeries = (n) =>
-  [...Array(n).keys()].reduce(
-    (acc, _r) => {
-      acc = [...acc, acc[acc.length - 1] + acc[acc.length - 2]];
-      return acc;
-    },
-    [0, 1]
-  );
+const fibSeries = () => {};
 
 // 13
 console.log(fibSeries(7));
