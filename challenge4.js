c82743c80f004fdacbb75415718dd31ea075ba5c
Phisit Siprasatthong <phisite@gmail.com>
2023-01-13 10:04:03 +0700

    feat: add solution for challenge4

--- challenge4.js
+++ challenge4.js
@@ -111,3 +111,75 @@ const orders = [
 //   },
 //   ...
 // ]
+
+// step 1 and 2
+
+const ordersWithOrderValues = orders.map((order) => {
+  const orderValue = order.items.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0
+  );
+  order.orderValue = orderValue;
+
+  return order;
+});
+
+// step 3 and 4
+
+const ordersWithShippingPrices = ordersWithOrderValues.map((order) => {
+  const matchedShippingTier = shippingByOrderValueTiers.find(
+    (tier) => order.orderValue < tier.orderValueLimit
+  );
+
+  order.shippingPrice = matchedShippingTier.shippingPrice;
+
+  order.totalValue = order.orderValue + order.shippingPrice;
+
+  return order;
+});
+
+console.log(JSON.stringify(ordersWithShippingPrices));
+
+// result
+// [
+//   {
+//     customer: "Bin",
+//     items: [
+//       { product: "Item 1", quantity: 1, price: 30 },
+//       { product: "Item 2", quantity: 2, price: 20 },
+//     ],
+//     orderValue: 70,
+//     shippingPrice: 30,
+//     totalValue: 100,
+//   },
+//   {
+//     customer: "Ploy",
+//     items: [
+//       { product: "Item 1", quantity: 20, price: 30 },
+//       { product: "Item 2", quantity: 5, price: 20 },
+//     ],
+//     orderValue: 700,
+//     shippingPrice: 20,
+//     totalValue: 720,
+//   },
+//   {
+//     customer: "Jam",
+//     items: [
+//       { product: "Item 1", quantity: 10, price: 30 },
+//       { product: "Item 2", quantity: 1, price: 20 },
+//     ],
+//     orderValue: 320,
+//     shippingPrice: 25,
+//     totalValue: 345,
+//   },
+//   {
+//     customer: "John",
+//     items: [
+//       { product: "Item 1", quantity: 100, price: 30 },
+//       { product: "Item 2", quantity: 100, price: 20 },
+//     ],
+//     orderValue: 5000,
+//     shippingPrice: 0,
+//     totalValue: 5000,
+//   },
+// ];
