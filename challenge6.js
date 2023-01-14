1509ba9f954e38e9c872cfc0dd02c421d2d3552d
Bond Rungrot <st122150@ait.asia>
2023-01-14 13:54:27 +0700

    init

--- challenge6.js
+++ challenge6.js
@@ -6,4 +6,8 @@ const socialUrl = {
 
 // This challenge is just FYI
 // Q1: get social channel and return url avoid using if/else or switch case
-console.log(socialUrl['FACEBOOK'])
+// console.log(socialUrl['FACEBOOK'])
+
+const channel = 'INSTAGRAM';
+const [, url] = Object.entries(socialUrl).find(([key]) => key === channel);
+console.log(url); // 'https://instagram.com'
