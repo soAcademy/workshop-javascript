0428632f0a055daa1dc0266771a90d8d7102aaa7
Your name <korayut@gmail.com>
2023-01-16 08:24:06 +0700

    Finished

--- challenge3.js
+++ challenge3.js
@@ -2,89 +2,47 @@ const customers = [
   { name: "John", age: 25, hobbies: ["reading", "hiking", "swimming"] },
   { name: "Jane", age: 32, hobbies: ["cooking", "dancing", "traveling"] },
   { name: "Bob", age: 28, hobbies: ["cycling", "painting", "swimming"] },
-  { name: "Alice", age: 22, hobbies: ["reading", "gardening"] }
-]
+  { name: "Alice", age: 22, hobbies: ["reading", "gardening"] },
+];
 
-// const result = customers.reduce((acc, c) => acc + c.name,"")
-// const result2 = customers.reduce((acc, c) => (acc+c.age),0)/customers.length
-// console.log(result2);
+//Quiz1 => By myself
+/*const filterCustomers = (customers) => {
+  const output = customers
+    .filter((f) => f.age >= 25 && f.age <= 30)
+    .map((a) => a.hobbies)
+    .flat();
+  // console.log(output);
+  const result = [...new Set(output)];
+  return result;
+};
+console.log("Customers hobbies 25-30 are", filterCustomers(customers));*/
 
-// const result3 = customers.reduce((acc, c) => {
-//   return {
-//     ...acc,
+//Ans Quiz2 => By myself
+/*const whoInterestSwimming = (customers) => {
+  const ans = customers
+    .filter((r) => r.hobbies.some((r) => r === `swimming`))
+    .map((r) => r.name);
+  return ans;
+};
+console.log("Person is interested swimming:", whoInterestSwimming(customers));*/
 
-//     hobbyCount: (acc.hobbyCount ?? 0) + c.hobbies.length,
-//     sumAge: (acc.sumAge ?? 0) + c.age,
-//   }
-// }, {})
-// console.log(result3);
-// {
-      // hobbyCount: 10,
-      // sumAge: 59
-// }
-
-// const sum = [1,2,3,4,5].reduce((acc,r)=> (acc+r)**2,0)
-// console.log(sum);
-
-//Ans Quiz1
-  // const output1 = customers.filter((f) =>f.age >= 25 && f.age <=30).map(a=>a.hobbies).flat();
-  // // console.log(output);
-  // const unique = [...new Set(output)];
-  // console.log(unique);
-
-  //Ans Quiz2 > indexof, include, some
-// const ans = customers.filter((r)=> r.hobbies.some((r)=> r===`swimming`)).map((r)=> r.name);
-// console.log(ans);
-
-//Ans Quiz3 (old)
-// const AnsQuiz3 = (customers) => {
-  // const items = customers.map((r)=> r.hobbies).flat();
-  // const hobbyCount = items.reduce(reduce
-// console.log(items);
-  // const unique = [...new Set(items)]
-  // // console.log(unique);
-  // const result = items.map((item) => {
-  //   const filterItem = unique.filter((u)=> u === items);
-  //   return {
-  //     item,
-  //   }
-  // })
-  
-  // console.log(hobby);
-  // const hobby = unique.filter(item => items === unique);
-  // const count = items.filter((r)=> r === hobby).length;
-  // const count = 
-  // console.log(reading);
-  // const hikingCount = items.filter((r)=> r === `hiking`).length;
-  // const swimmingCount = items.filter((r)=> r === `swimming`).length;
-  // const cookingCount = items.filter((r)=> r === `cooking`).length;
-  // const dancingCount = items.filter((r)=> r === `dancing`).length;
-  // const travelingCount = items.filter((r)=> r === `traveling`).length;
-  // const cyclingCount = items.filter((r)=> r === `cycling`).length;
-  // const paintingCount = items.filter((r)=> r === `painting`).length;
-  // const gardeningCount = items.filter((r)=> r === `gardening`).length;
-  // // return [
-  // //   {
-  //     hobby: "hiking",
-  //     count: hikingCount
-  //   }
-  // ]
-
-// return{
-//   hobby,
-// //   count,
-// }
-
-// }
-
-// console.log(AnsQuiz3(customers))
-
-
-//Ans Quiz3
-const items = customers.map((r)=> r.hobbies).flat();
-
-
-
+//Ans Quiz3 => By myself
+const countCustomerByHobby = (customers) => {
+  let hobbyCount = [];
+  const hobbiesDetail = customers.map((h) => h.hobbies);
+  for (let i = 0; i < hobbiesDetail.length; i++) {
+    hobbyCount = hobbyCount.concat(hobbiesDetail[i]);
+  }
+  const colorCount = hobbyCount.reduce((count, r) => {
+    count[r] = (count[r] || 0) + 1;
+    return count;
+  }, {});
+  const result = Object.entries(colorCount).forEach(([hobby, count]) => {
+    console.log(`hobby: "${hobby}", count: ${count}`);
+  });
+  return result;
+};
+console.log(countCustomerByHobby(customers));
 
 // Quiz 1: Filter customer age >= 25 and age <= 30 and then get unique hobbies
 // ["reading", "hiking", "swimming", "cycling", "painting"]
@@ -109,4 +67,3 @@ const items = customers.map((r)=> r.hobbies).flat();
 //   },
 //   ...
 // ]
-
